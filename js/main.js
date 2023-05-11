ebf067927098cce7824c031ac959e051850bd712
MirkoGLostia <giggi.torre@mail.com>
2023-05-11 13:14:04 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+1) Visualizzare in pagina 5 numeri casuali.
+
+2) Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono 
+
+3) l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+4) Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
